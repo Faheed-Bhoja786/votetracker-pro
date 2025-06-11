f5d67c7f0eaa0f713d46580eb7941723d1bf3666
Faheed-Bhoja786 <polestar2103@gmail.com>
2025-06-11 18:00:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,32 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Example database model (we'll expand it later)
+# Models directly inside app for now
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
 class Voter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     voted = db.Column(db.Boolean, default=False)
 
+class Party(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    logo = db.Column(db.String(200))
+
+class Vote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    voter_id = db.Column(db.Integer)
+    party_id = db.Column(db.Integer)
+
 @app.route('/')
 def home():
-    return "VoteTrackerPro is working!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
